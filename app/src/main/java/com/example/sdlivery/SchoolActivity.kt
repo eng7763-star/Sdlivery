136419acf6eafef8b4cafd0fb0d8053cc7caec88
eng7763-star <eng7763@gmail.com>
2025-09-05 12:03:12 +0300

    إنشاء ملف SchoolActivity.kt

---
+++ app/src/main/java/com/example/sdlivery/SchoolActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sdlivery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SchoolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.school_screen)
+        // يمكنك إضافة منطق التعامل مع عناصر الشاشة هنا
+    }
+}
