014989f9bb53ac25b38f18f6ccfc191dd2cdba95
eng7763-star <eng7763@gmail.com>
2025-09-05 12:03:05 +0300

    إنشاء ملف DriverActivity.kt

---
+++ app/src/main/java/com/example/sdlivery/DriverActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sdlivery
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DriverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.driver_screen)
+        // يمكنك إضافة منطق التعامل مع عناصر الشاشة هنا
+    }
+}
